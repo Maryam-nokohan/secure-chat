bdaae48c5f41aadb623238e3ed8db770eab09b4a
mary <maryam.nokohan@gmail.com>
2026-05-27 11:10:48 +0330

    feat : added context to the user port

---
+++ internal/core/ports/user.go
@@ -0,0 +1,8 @@
+package ports
+
+import "context"
+
+type UserServicesI interface {
+	Register(ctx context.Context, username, password string) error
+	Login(ctx context.Context, username, password string) (string, error)
+}
