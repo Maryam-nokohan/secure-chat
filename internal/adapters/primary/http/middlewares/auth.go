d862ef574aa5be4a0f4e50c146ab30badbbdb870
mary <maryam.nokohan@gmail.com>
2026-05-27 12:59:54 +0330

    feat [security] : added the auth middleware

---
+++ internal/adapters/primary/http/middlewares/auth.go
@@ -0,0 +1,38 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/maryam-nokohan/secure-chat/internal/core/ports"
+)
+
+
+func AuthMiddleware(tokenSvc ports.TokenService) gin.HandlerFunc{
+	return func(c *gin.Context) {
+		token , err := c.Cookie("Authorization")
+
+		if err != nil {
+			c.Redirect(
+				http.StatusSeeOther,
+				"/login",
+			)
+			c.Abort()
+			return 
+		}
+		claims , err := tokenSvc.Validate(token)
+		
+		if err != nil {
+			c.Redirect(
+				http.StatusSeeOther,
+				"/login",
+			)
+			c.Abort()
+			return 
+		}
+
+		c.Set("userID" , claims.UserID)
+		c.Set("username" , claims.Username)
+	}
+
+}
